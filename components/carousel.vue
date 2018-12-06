f0d072c62f60a11402a0a6ac6f59b787a819b6ad
AMarko <btmac@MAC.local>
2018-12-06 17:20:36 +0000

    added new code

--- components/carousel.vue
+++ components/carousel.vue
@@ -5,14 +5,14 @@
           <div class="video_shader"></div>
           <div class="video_contain">
            <video class="video-player" id="background" preload="auto" width="100%"
-              poster="~/assets/videos/cityjsconf.jpg"
+              poster="/videos/cityjsconf.jpg"
               muted playsinline="" height="100%" aria-labelledby="banner381127-text" autoplay loop>
-                <source src="~/assets/videos/cityjs.mp4" type="video/mp4">
-                <source src="~/assets/videos/cityjs.webm" type="video/webm">
+                <source src="/videos/cityjs.mp4" type="video/mp4">
+                <source src="/videos/cityjs.webm" type="video/webm">
                 <!-- <source src="~/assets/videos/cityjsconf.ogv" type="video/ogg"> -->
                 <p>Sorry, your browser doesn't support embedded videos, 
                    but you can 
-                   <a href="~/assets/videos/cityjs.mp4">download it</a>
+                   <a href="/videos/cityjs.mp4">download it</a>
                     and watch it with your favorite video player
                 </p>
             </video>
--- components/speakers2019.vue
+++ components/speakers2019.vue
@@ -15,28 +15,43 @@
                 v-bind:key="item._id"
             >
                 <div class="card">
-                    <div class="card-image position-relative">
-                        <figure class="image is-40x40">
-                            <img 
-                                v-if="typeof 
-                                    item.image!== 'undefined'"  
-                                    width="200" :alt="item.name" 
-                                    :src="`//api.spiralthread.com/${item.thumbnail.path}`"
-                                    class="speaker"
-                            />
-                        </figure>
-                        <figcaption class="label label-small bg-blue bottom-0 m-3 position-absolute right-0 text-uppercase text-white">
-                            {{item.name}}
-                        </figcaption>
-                    
-                    </div>
-                    <!-- <div class="back-card content">
-                            <div class="media-content">
-                                <h2 class="title is-4">{{item.name}}</h2>
-                                <p><span class="title is-6"><a href="http://twitter.com/${item.twitter}">@{{item.twitter}}</a></span></p>
-                                <p class="subtitle is-6">{{item.company}}</p>
+                    <div class="card-image position-relative flip-container">
+                        <div class="flipper">
+                            <div class="front">
+                                <figure class="image is-40x40">
+                                    <img 
+                                        v-if="typeof 
+                                            item.image!== 'undefined'"  
+                                            width="200" :alt="item.name" 
+                                            :src="`//api.spiralthread.com/${item.thumbnail.path}`"
+                                            class="speaker"
+                                    />
+                                </figure>
+                                <figcaption class="label label-small bg-blue bottom-0 m-3 position-absolute right-0 text-uppercase text-white">
+                                    {{item.name}}
+                                </figcaption>
                             </div>
-                    </div> -->
+                            <div class="back">
+                                 <figure class="back-logo">
+                                    <img 
+                                        v-if="typeof 
+                                            item.image!== 'undefined'"  
+                                            width="200" :alt="item.name" 
+                                            :src="`//api.spiralthread.com/${item.thumbnail.path}`"
+                                            class="speaker"
+                                    />
+                                </figure>
+                                <div class="back-card content">
+                                    <div class="media-content back-content">
+                                        <h2 class="title is-4">{{item.name}}</h2>
+                                        <p><span class="title is-6"><a href="http://twitter.com/${item.twitter}">@{{item.twitter}}</a></span></p>
+                                        <p class="subtitle is-6">{{item.company}}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!--  -->
                 </div>
             </div>
         </div>
@@ -105,4 +120,56 @@ export default {
     .label
         width: 50%;
         padding: 20px;
+
+    .flip-container
+        @include per(1000);
+        &:hover
+            .flipper
+                transform: rotateY(180deg)
+        .flipper 
+            transition: 0.6s;
+            transform-style: preserve-3d;
+            position: relative;
+    
+    .front, .back
+        backface-visibility: hidden;
+        position: absolute;
+        top: 0;
+        left: 0;
+    
+
+    .flip-container, .front, .back
+        width: 100%;
+        height: 500px;
+
+    /* front pane, placed above back */
+    .front
+        z-index: 2;
+        /* for firefox 31 */
+        transform: rotateY(0deg);
+
+    /* back, initially hidden pane */
+    .back
+        transform: rotateY(180deg);
+    
+    .back-logo
+        position: absolute;
+        top: 40px;
+        left: 37%;
+        width: 150px;
+        height: 150px;
+        border-radius: 50%;
+
+    .back-content
+        padding-top: 40px;
+        font-weight: bold;
+        color: #00304a;
+        position: absolute;
+        top: 180px;
+        left: 0;
+        right: 0;
+        text-align: center;
+        font-family: Courier;
+        font-size: 22px;
+
 </style>
