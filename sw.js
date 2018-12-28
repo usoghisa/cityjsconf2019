9aaaf684646e105b94f38cf675209413fb228d56
Aris Markogiannakis <aristos.markogiannakis@bt.com>
2018-12-28 12:22:52 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,24 +2,24 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/28716f7a3ee7f215f835.js",
-    "revision": "6d1d651660873bc30aa39e183ca8cfba"
-  },
-  {
-    "url": "/_nuxt/384dd9197c5167226ea4.js",
-    "revision": "0f5835d9b13a66f84b1483427012043c"
+    "url": "/_nuxt/373589d7a099313dd1fa.js",
+    "revision": "497f3cbb8c64a74c5a0f1ad187733800"
   },
   {
     "url": "/_nuxt/502383e5fa12a850ae86.js",
     "revision": "8a33e71b0e9ea5d0c7807e5d7fe7f33c"
   },
   {
-    "url": "/_nuxt/e42563a6c04ead71accf.js",
-    "revision": "c9e2afbffd7a4caaadc778f7ca0058ea"
+    "url": "/_nuxt/9c87524a660880d4dc8a.js",
+    "revision": "bb596306906db2ddc15d6f09b01f6712"
   },
   {
-    "url": "/_nuxt/ffc8c21a78eb148e4601.js",
-    "revision": "4bff17054d8380fc47eef9fc891deb82"
+    "url": "/_nuxt/b3b2d92bf4643d5b12ad.js",
+    "revision": "fe4854b23e88a347c1ea64d7a7d36538"
+  },
+  {
+    "url": "/_nuxt/e42563a6c04ead71accf.js",
+    "revision": "c9e2afbffd7a4caaadc778f7ca0058ea"
   }
 ], {
   "cacheId": "cityjsconf",
