b4ad13302c320d4e13af308f985dcd86cb7c2917
AMarko <btmac@MAC.local>
2019-01-11 13:23:32 +0000

    added more code

---
+++ components/day.vue
@@ -0,0 +1,54 @@
+<template>
+    <section class="hero columns">
+      <div class="container"> 
+          {{dates}}
+        <div class="tabs">
+             <ul>
+                <li 
+                    v-for="(date) in dates"
+                    v-bind:key="date"
+                    class="is-active">
+                    <a v-on:click="select(date.title)">{{date.title}}</a>
+                </li>
+            </ul>
+        </div>
+      </div>
+    </section>
+</template>
+
+<script>
+    import h2 from '@/components/h2';
+
+    export default {
+        props: {
+            items: Array,
+            day: String,
+        },
+        components: {
+            "app-h2" : h2
+        },
+        methods: {
+            select: function (date) {
+                alert(date)
+            }
+        },
+        computed: {
+            dates() {
+              if (typeof this.items!== 'undefined') {
+                    return this.items.filter((date) => {
+                        return date.date === this.day;
+                    });
+              } else {
+                  return [];
+              }
+            
+            }
+        }
+    };
+</script>
+
+<style lang="sass" scoped>
+  @import '~/assets/css/mq.sass';
+
+
+</style>
--- components/schedule.vue
+++ components/schedule.vue
@@ -10,19 +10,47 @@
             </app-h2>
         </div>
       </div>
-          {{Days}}
+      <div class="container"> 
+        <div class="tabs">
+            <ul>
+                <li 
+                    v-for="(date, index) in Days"
+                    v-bind:key="date"
+                    class="is-active">
+                    <a v-on:click="select(date)">Day {{index+1}} </a>
+                </li>
+            </ul>
+        </div>
+        <app-day
+            :items= 'items'
+            :day= 'selectedDate'
+        >
+        </app-day>
+      </div>
     </section>
 </template>
 
 <script>
     import h2 from '@/components/h2';
+    import day from '@/components/day';
 
     export default {
+        data: function() {
+            return {
+              selectedDate: {},
+            }
+        },
         props: {
             items: Array,
         },
         components: {
-            "app-h2" : h2
+            "app-h2" : h2,
+            "app-day": day
+        },
+        methods: {
+            select: function (date) {
+                this.selectedDate = date;
+            }
         },
         computed: {
             Days() {
@@ -31,8 +59,7 @@
               } else {
                   return [];
               }
-            
-            }
+            },
         }
     };
 </script>
