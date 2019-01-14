423aaee1d835581eef730fbf784855688a5f23b7
arismarko <aristos_m@hotmail.com>
2019-01-14 21:40:26 +0000

    added

--- components/day.vue
+++ components/day.vue
@@ -6,8 +6,8 @@
         <aside class="menu">
             <ul class="menu-list">
                <li 
-                    v-for="(date) in dates"
-                    v-bind:key="date"
+                    v-for="(date, index) in dates"
+                    v-bind:key="index"
                     class="is-active">
                     <a v-on:click="select(date)">{{date.title}}</a>
                 </li>
@@ -15,7 +15,10 @@
         </aside>
       </div>
       <div class="column is-9"> 
-          {{selectedTalk}}
+        <app-talk
+            :talk= 'talk'
+        >
+        </app-talk>
       </div>
       </div>
       </div>
@@ -24,22 +27,25 @@
 
 <script>
     import h2 from '@/components/h2';
+    import talk from '@/components/talk';
 
     export default {
-        data: {
-            selectedTalk: {}
+        data: function() {
+            return {
+              talk: {}
+            } 
         },
         props: {
             items: Array,
             day: String,
         },
         components: {
-            "app-h2" : h2
+            "app-h2" : h2,
+            "app-talk" : talk
         },
         methods: {
             select: function (date) {
-                console.log(date);
-               this.selectedTalk = date;
+               this.talk = date;
             }
         },
         computed: {
@@ -52,7 +58,7 @@
                   return [];
               }
             
-            }
+            },
         }
     };
 </script>
---
+++ components/talk.vue
@@ -0,0 +1,31 @@
+<template>
+    <section class="section">
+      <div class="container">
+      <div class="columns">
+      <div class="column is-3"> 
+       {{talk}}
+      </div>
+      <div class="column is-9"> 
+          
+      </div>
+      </div>
+      </div>
+    </section>
+</template>
+
+<script>
+    import h2 from '@/components/h2';
+
+    export default {
+        props: {
+            talk: Object,
+        },
+        components: {
+            "app-h2" : h2
+        }
+    };
+</script>
+
+<style lang="sass" scoped>
+  @import '~/assets/css/mq.sass';
+</style>
