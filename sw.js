1ea14eab5ba22ecf6bbf09e4c6cbac0c15d30169
Aris Markogiannakis <aristos_m@hotmail.com>
2019-02-07 23:35:50 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,24 +6,24 @@ workbox.precaching.precacheAndRoute([
     "revision": "a2e27bacfdabfe6e26ec680c8b559dbf"
   },
   {
-    "url": "/_nuxt/176d2b931c8bb869e6ae.js",
-    "revision": "b0966781568ddd46b95e938b8ffd7fbe"
-  },
-  {
     "url": "/_nuxt/3f594ee72faab29e9173.js",
     "revision": "3f61105c450bd791a129a27e189c0171"
+  },
+  {
+    "url": "/_nuxt/5d842fe791b7d0ed0017.js",
+    "revision": "c4b28bf6533c5fcdb4590ccf25c66646"
   },
   {
     "url": "/_nuxt/b39f9ef856533888f2b3.js",
     "revision": "b4c97ff36b6d361ca0b6d722dd925058"
   },
   {
-    "url": "/_nuxt/c58501768266b06cae90.js",
-    "revision": "62dd50737eca8fccd009db69346d4d7b"
-  },
-  {
     "url": "/_nuxt/d64ba63fd732b98abf9d.js",
     "revision": "66f5e84fb257b7f42d00afbf15f6c3b1"
+  },
+  {
+    "url": "/_nuxt/f72603ed4df8df197595.js",
+    "revision": "8af10889beb813f19421a5f7bcc2fd11"
   }
 ], {
   "cacheId": "cityjsconf",
