b5e3ecb42578a963e40fc626f26bd91f3731543b
arismarko <aristos_m@hotmail.com>
2019-02-15 14:19:50 +0000

    add all rest of work

--- components/payments.vue
+++ components/payments.vue
@@ -3,7 +3,11 @@
         <link rel="stylesheet" type="text/css" href='https://css.tito.io/v1.1' />
         <script src='https://js.tito.io/v1' async></script>
         <tito-widget event="cityjsconf-london/cityjsconf-2019/">
-              <img class="sponsor" alt="sponsor"  src="tenor.gif">
+              <img class="sponsor" alt="sponsor"  src="tenor.gif"> <br/>
+
+             
         </tito-widget>
+        <p><a href="https://ti.to/cityjsconf-london/cityjsconf-2019">Buy tickets or get Free workshops</a> if the above is not loading</p>
+        
     </div>
 </template>
--- static/sw.js
+++ static/sw.js
@@ -6,12 +6,12 @@ workbox.precaching.precacheAndRoute([
     "revision": "5fb9de1250485f643278b4abe54880f6"
   },
   {
-    "url": "/_nuxt/5afe51b481f925267f56.js",
-    "revision": "4b7170de465aebb453974eba3c26e12c"
+    "url": "/_nuxt/32f057a8ac1364e755d8.js",
+    "revision": "17d3a2a0ce2defab2df783ff56f9fa99"
   },
   {
-    "url": "/_nuxt/734320a0ee6acc956578.js",
-    "revision": "565e0ce2d45183da6d3fd0b5d2c08a55"
+    "url": "/_nuxt/5afe51b481f925267f56.js",
+    "revision": "4b7170de465aebb453974eba3c26e12c"
   },
   {
     "url": "/_nuxt/93cd44d7b9f5b0de36e1.js",
@@ -22,8 +22,8 @@ workbox.precaching.precacheAndRoute([
     "revision": "b4c97ff36b6d361ca0b6d722dd925058"
   },
   {
-    "url": "/_nuxt/c0266b3891fee45342fd.js",
-    "revision": "7288551ec2e1943f217b436037d31bed"
+    "url": "/_nuxt/c00c1bfc85cb965a3943.js",
+    "revision": "75d5f13d95fd945ead8940b53caff44e"
   }
 ], {
   "cacheId": "cityjsconf",
