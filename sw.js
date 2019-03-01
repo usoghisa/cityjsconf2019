7397560c4eef85f9b04d7003efad2e48ba1810a0
Aris Markogiannakis <aristos_m@hotmail.com>
2019-03-01 22:56:35 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -6,6 +6,10 @@ workbox.precaching.precacheAndRoute([
     "revision": "4da82027b65a83b56afb62f098d379e2"
   },
   {
+    "url": "/_nuxt/2847d4777742243ceb18.js",
+    "revision": "bdfc7ee3fc58aa39ebb67909f084bcac"
+  },
+  {
     "url": "/_nuxt/2ee401242768f4931bb4.js",
     "revision": "b6af5255e5e092b8e84ce0b5ad979242"
   },
@@ -16,10 +20,6 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "/_nuxt/93cd44d7b9f5b0de36e1.js",
     "revision": "654572edf4c1b8729629a8302787936d"
-  },
-  {
-    "url": "/_nuxt/a0dce059d28c89322b92.js",
-    "revision": "301074b18015ebd7cd3152522d9a747e"
   },
   {
     "url": "/_nuxt/b39f9ef856533888f2b3.js",
