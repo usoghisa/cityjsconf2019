885331fbfcae7184df330f25b4b8b5b329ca501a
Aris Markogiannakis <aristos_m@hotmail.com>
2019-03-22 22:50:26 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -2,12 +2,8 @@ importScripts('/_nuxt/workbox.4c4f5ca6.js')
 
 workbox.precaching.precacheAndRoute([
   {
-    "url": "/_nuxt/092c84a924fd4da21a44.js",
-    "revision": "6399da9ae7b7fdb5884d2d7a47e93f61"
-  },
-  {
-    "url": "/_nuxt/2fd88580221779caaf08.js",
-    "revision": "bf971f58ff3c62ab59e66e37170b69b7"
+    "url": "/_nuxt/298e11491ffcc4272742.js",
+    "revision": "5c33ee3e3ea68a4eb39c7b990f8638a7"
   },
   {
     "url": "/_nuxt/c0ea7f1700fef4b93150.js",
@@ -16,6 +12,10 @@ workbox.precaching.precacheAndRoute([
   {
     "url": "/_nuxt/c1ee2ea30db0e97796e8.js",
     "revision": "1a00c9d27ec8e5b5c184b01e95537f9c"
+  },
+  {
+    "url": "/_nuxt/d852fade06e707136a7d.js",
+    "revision": "251651e86d9f27966764d2ad18a5a44f"
   },
   {
     "url": "/_nuxt/e9761866d65dd4694f2e.js",
