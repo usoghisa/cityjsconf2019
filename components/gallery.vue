fe36cb1472a828e657efe70af4c980ce44fea8e0
Dragan Ceran <dragandceran@gmail.com>
2019-09-04 16:04:26 +0100

    gallery redisgn

--- components/gallery.vue
+++ components/gallery.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="blackbag">
       <div class="container">
-         <div class="row"> 
+         <div class="row">
             <app-h2
                 title="Our Gallery"
                 subtitle="join us this year"
@@ -9,21 +9,64 @@
                 :white="true"
             >
             </app-h2>
-            <div class="columns is-variable is-3-mobile is-0-tablet is-8-desktop is-8-widescreen is-8-fullhd">
-                <div class="column">
+                <div class="columns">
+                  <div class="section">
+                    <div class="polaroid">
+                      <img src="/images/previous_year/elle.jpg">
+                        <div class="container">
+                          <p>ELLE</p>
+                        </div>
+                      </div>
+                    <div class="polaroid">
+                      <img src="/images/previous_year/audience.jpg">
+                        <div class="container">
+                          <p>AUDIENCE</p>
+                        </div>
+                    </div>
+                    <div class="polaroid">
+                      <img src="/images/previous_year/elle.jpg">
+                        <div class="container">
+                          <p>ELLE</p>
+                        </div>
+                    </div>
+                </div>
+                <!-- <div class="column">
+                <div class="polaroid">
+                  <img src="/images/previous_year/elle.jpg">
+                    <div class="container">
+                      <p>ELLE</p>
+                    </div>
+                  </div>
+                <div class="polaroid">
+                  <img src="/images/previous_year/audience.jpg">
+                    <div class="container">
+                      <p>AUDIENCE</p>
+                    </div>
+                </div>
+                <div class="polaroid">
+                  <img src="/images/previous_year/elle.jpg">
+                    <div class="container">
+                      <p>ELLE</p>
+                    </div>
+                </div>
+              </div> -->
+                </div>
+
+</div>
+                <!-- <div class="column">
                     <img src="/images/previous_year/audience.jpg">
                     <img src="/images/previous_year/cinemas.jpg">
                     <img src="/images/previous_year/elle.jpg">
                 </div>
                 <div class="column" >
-                    <img src="/images/previous_year/cinemas.jpg"> 
+                    <img src="/images/previous_year/cinemas.jpg">
                     <img src="/images/previous_year/audience.jpg">
                     <img src="/images/previous_year/elle.jpg">
-                </div> 
+                </div>
                 <div class="column">
                     <img src="/images/previous_year/dylan.jpg">
-                     <img src="/images/previous_year/cinemas.jpg"> 
-                    <img src="/images/previous_year/audience2.jpg">
+                     <img src="/images/previous_year/cinemas.jpg">
+                    <img src="/images/previous_year/audience2.jpg"> -->
                 </div>
             </div>
         </div>
@@ -52,18 +95,36 @@
 
 <style lang="sass" scoped>
     @import '~/assets/css/mq.sass';
-    
-    .nopadding 
-        padding: 0
 
-    
+    .tile
+        background-color: rgba(255, 48, 48, 0.9);
+
+    .nopadding
+        padding: 0;
+
+
     .blackbag
         background: $black;
         padding: 20px;
 
-    .columns
+    .section
         display: flex;
-        flex-direction: row; // this is default
-        align-items: stretch; // this will stretch the children vertically
+        flex-direction: row// this is default;
+
+
+    .polaroid
+      width: 80%
+      background-color: white;
+      margin: 10px;
+
+
+    img
+      width: 100%;
+
+    .container
+      text-align: center;
+      padding: 10px 20px;
+
+
 
 </style>
