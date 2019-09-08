8bb04a159a3065e22aa0b11031beeda93dfd7f90
Markogiannakis Aris <x1227377@MACLA217312.local>
2019-09-08 22:02:58 +0100

    added

--- components/gallery.vue
+++ components/gallery.vue
@@ -2,7 +2,7 @@
   <section class="blackbag">
     <div class="container">
       <div class="row">
-        <app-h2 title="Our Gallery" subtitle="join us this year" :is-h2="true" :white="true"></app-h2>
+        <app-h2 title="Our Gallery" subtitle="some of the best moments" :is-h2="true" :white="true"></app-h2>
         <div class="columns is-multiline">
           <div class="column is-4 is-12-mobile">
             <a
@@ -12,7 +12,7 @@
             >
               <img class="is-2" src="/images/previous_year/audience.jpg" />
               <div class="pictitle">
-                <p>Audience</p></p>
+                <p>Audience</p>
               </div>
             </a>
           </div>
@@ -34,7 +34,7 @@
               href="https://www.flickr.com/photos/182802514@N02/albums/72157710741317572"
               target="_blank"
             >
-              <img src="/images/previous_year/audience2.jpg" />
+              <img src="/images/previous_year/cinemas.jpg" />
               <div class="pictitle">
                 <p>CityJS 2019</p>
               </div>
@@ -70,12 +70,19 @@
               href="https://www.flickr.com/photos/182802514@N02/albums/72157710740775736"
               target="_blank"
             >
-              <img src="/images/previous_year/elle.jpg" />
+              <img src="/images/previous_year/workshops.jpeg" />
               <div class="pictitle is-h2">
                 <p>Workshops</p>
               </div>
             </a>
           </div>
+        </div>
+
+        <div class="is-12">
+          <p class="is-pulled-left photography">
+            photography
+            <a href="https://www.instagram.com/mnmarkg/">@mnmarkg</a>
+          </p>
         </div>
       </div>
     </div>
@@ -83,7 +90,6 @@
 </template>
 
 <script>
-
 import h2 from "@/components/h2";
 export default {
   data: function() {
@@ -95,8 +101,7 @@ export default {
     "app-h2": h2
   },
   computed: {},
-  props: {
-  }
+  props: {}
 };
 </script>
 
@@ -125,6 +130,15 @@ export default {
         +mobile
          float: left;
          width: 24%;
+
+        
+    .photography
+      color: $white;
+      font-size: 12px;
+      margin-left: 20px;
+      a
+        color: $white;
+        font-weight: bold;
 
 
     .pictitle
--- components/highlights.vue
+++ components/highlights.vue
@@ -1,90 +1,102 @@
 <template>
-    <section class="section bg-red">
-    <div class="" id="videos">
-        <app-h2
-            :title="`Our ${current.year} Highlights`"
-            :subtitle="`View from our amazing ${this.year} speakers (sponsored by Pusher)`"
-            :is-h2="true"
-            :white="true"
-        >
-        </app-h2>
-        <div class="columns">
-            <div class="column">  
-                <div class='carousel carousel-animated carousel-animate-slide' :data-size="isMobile ? '1' : '3'" data-autoplay="true">
-                    <div class='carousel-container'>
-                        <div class='carousel-item has-background is-active'
-                            v-for="item in speakers"
-                            v-bind:key="item._id"
-                        >
-                            <a class="speaker" :href="`${item.videoUrl}`">
-                                <figure class="bimage">
-                                    <img 
-                                        v-if="typeof item.image!== 'undefined'" 
-                                        :alt="item.name"  
-                                        :src="`//api.spiralthread.com/${item.image.path}`"
-                                    >
-                                </figure>
-                                <div class="media-content">
-                                    <img src="/images/play-btn.svg" class="play" alt="play" width="40" />
-                                    <p class="is-4-mobile no-padding">{{item.name}}</p>
-                                     <p class="subtitle is-6-mobile">{{item.company}}</p>
-                                    <p><span class=" is-6-mobile">{{item.title}}</span></p>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="carousel-navigation is-centered">
-                        <div class="carousel-nav-left">
-                            <i class="fa fa-chevron-left" aria-hidden="true"></i>
-                        </div>
-                        <div class="carousel-nav-right">
-                            <i class="fa fa-chevron-right" aria-hidden="true"></i>
-                        </div>
-                    </div>
-                </div>
+  <section class="section bg-red">
+    <div class id="videos">
+      <app-h2
+        :title="`Our ${current.year} Highlights`"
+        :subtitle="`View from our amazing ${this.year} speakers (sponsored by Pusher)`"
+        :is-h2="true"
+        :white="true"
+      ></app-h2>
+      <div class="columns">
+        <div class="column">
+          <div
+            class="carousel carousel-animated carousel-animate-slide"
+            :data-size="isMobile ? '1' : '3'"
+            data-autoplay="true"
+          >
+            <div class="carousel-container">
+              <div
+                class="carousel-item has-background is-active"
+                v-for="item in speakers"
+                v-bind:key="item._id"
+              >
+                <a class="speaker" :href="`${item.videoUrl}`">
+                  <figure class="bimage">
+                    <img
+                      v-if="typeof item.image!== 'undefined'"
+                      :alt="item.name"
+                      :src="`//api.spiralthread.com/${item.image.path}`"
+                    />
+                  </figure>
+                  <div class="media-content">
+                    <img src="/images/play-btn.svg" class="play" alt="play" width="40" />
+                    <p class="is-4-mobile no-padding">{{item.name}}</p>
+                    <p class="subtitle is-6-mobile">{{item.company}}</p>
+                    <p>
+                      <span class="is-6-mobile">{{item.title}}</span>
+                    </p>
+                  </div>
+                </a>
+              </div>
             </div>
-         </div>
+            <div class="carousel-navigation is-centered">
+              <div class="carousel-nav-left">
+                <i class="fa fa-chevron-left" aria-hidden="true"></i>
+              </div>
+              <div class="carousel-nav-right">
+                <i class="fa fa-chevron-right" aria-hidden="true"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    </section>
+  </section>
 </template>
 
 <script>
 import bulmaCarousel from "bulma-extensions/bulma-carousel/dist/js/bulma-carousel.min.js";
 import "bulma-extensions/bulma-carousel/dist/css/bulma-carousel.min.css";
-import h2 from '@/components/h2';
-import { mapGetters } from 'vuex';
+import h2 from "@/components/h2";
+import { mapGetters } from "vuex";
 
 export default {
   props: {
-     year: String
+    year: String
   },
   components: {
-    'app-h2': h2,
+    "app-h2": h2
   },
   mounted: function() {
     this.carousel = bulmaCarousel.attach();
   },
   computed: {
-    isMobile () {
-        return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ? true : false;
+    isMobile() {
+      return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+        navigator.userAgent
+      )
+        ? true
+        : false;
     },
     ...mapGetters({
-      items: 'speakers/speakers',
-      current: 'pages/current',
+      items: "speakers/speakers",
+      current: "pages/current"
     }),
-    speakers () {
-        if (typeof this.items!== 'undefined') {
-         return this.items.filter(item => {
-             return (item.year === parseInt(this.current.year)) 
-             && (item.event === "talk" || item.event === "both")
-         }); 
-        } else {
-            return [];
-        }
+    speakers() {
+      if (typeof this.items !== "undefined") {
+        return this.items.filter(item => {
+          return (
+            item.year === parseInt(this.current.year) &&
+            (item.event === "talk" || item.event === "both")
+          );
+        });
+      } else {
+        return [];
+      }
     }
   },
-  created (store) {
-    this.$store.dispatch('speakers/get');
+  created(store) {
+    this.$store.dispatch("speakers/get");
   }
 };
 </script>
@@ -135,7 +147,7 @@ export default {
         margin-top: -100px;
         position: absolute;
         background-color: $white;
-        width: 93%;
+        width: 94%;
         padding: 10px;
         height: 200px;
         .title 
