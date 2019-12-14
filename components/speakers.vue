c3fe34555635883a009cf7cbcc3d49b2703c0f6c
Aris(WiproDigital) <ariswiprodigital@ariswipitalsmbp.home>
2019-12-14 16:27:16 +0000

    speakers

--- components/speakers.vue
+++ components/speakers.vue
@@ -1,80 +1,136 @@
 <template>
-    <section class="section shallow">
-    <div class="testimonials has-text-centered">
+    <div class="container">
+    <section class="section shallow is-centered">
+    <div class="testimonials is-text  ">
         <app-h2
             title="2019 Speakers"
-            subtitle="We are excited  to announce our selected Speakers <br/> for more information check our <a href='/speakers'>speakers</a> section"
+            subtitle="We are excited  to announce our talks for this year"
             :is-h2="true"
         >
         </app-h2>
-        <div class="row columns is-mobile is-multiline"> 
-            <div 
-                class="column is-one-quarter-desktop is-full-mobile"  
+        <div class="columns  is-mobile is-multiline is-centered"> 
+           <div 
+                class="column is-three-quarters-mobile is-two-thirds-tablet
+                 is-half-desktop is-one-third-widescreen is-one-quarter-fullhd"
                 v-for="item in filteredSpeakers"
                 v-bind:key="item._id"
-            >
-                <div class="card">
-                    <div class="card-image position-relative flip-container">
-                        <div class="flipper">
-                            <div class="front">
-                                <figure class="image is-40x40">
+           >
+                    <div class="is-one  ">
+                         <div class="card-image">
+                            <a 
+                                v-on:click="select(item)"
+                            >
+                                <figure class="image is-92x92">
                                     <img 
                                         v-if="typeof 
-                                            item.image!== 'undefined'"  
-                                            width="150" :alt="item.name" 
-                                            :src="`/siteimages/${item.thumbnail.path}`"
-                                            class="speaker"
+                                        item.image!== 'undefined'"  
+                                        :alt="item.name" 
+                                        :src="`/siteimages/${item.thumbnail.path}`"
+                                        class="speaker"
                                     />
                                 </figure>
-                                <figcaption class="label label-small bg-blue top-100 m-3 position-absolute right-0 text-uppercase text-white">
-                                    {{item.name}}
-                                </figcaption>
-                            </div>
-                            <div class="back">
-                                 <figure class="back-logo">
-                                    <img 
-                                        v-if="typeof 
-                                            item.image!== 'undefined'"  
-                                            :alt="item.name" 
-                                            :src="`/siteimages/${item.thumbnail.path}`"
-                                            class="speaker"
-                                    />
-                                </figure>
-                                <div class="back-card content">
-                                    <div class="media-content back-content">
-                                        <p class="title is-4">{{item.name}}</p>
-                                        <p><span class="title is-6"><a :href="`//twitter.com/${item.twitter}`">@{{item.twitter}}</a></span></p>
-                                        <p class="subtitle is-6">{{item.company}}</p>
-                                        <p v-html="item.title" />
-                                        <a href="/speakers"> Read more </a>
-                                    </div>
-                                    
+                            </a>
+                        </div>
+                        <div class="card-content">
+                            <div class="media">
+                                <div class="media-content has-text-centered">
+                                     <a 
+                                        class="titlebtn"
+                                        v-on:click="select(item)"
+                                    >
+                                        <h3 class="small-title">
+                                            {{item.title}}                                                                  
+                                        </h3>
+                                    </a>
+                                    <p class="subtitle is-6">{{item.name}}  <a class="icon" :href="`//twitter.com/${item.twitter}`"><i class="fa fa-twitter"></i></a>   
+                                       <br/>  {{item.company}}
+                                     </p>  
                                 </div>
                             </div>
                         </div>
                     </div>
+           </div>
+           <div 
+                :class="['column spekers-card-container modal-background', {'closed':open === false}]" 
+                v-if="this.chosen"
+            >
+                <div class="card speaker-card">
+                    <div class="card-content">
+                        <div class="media">
+                        <div class="media-left">
+                            <figure class="image is-48x48">
+                                <img 
+                                    v-if="typeof 
+                                    chosen.image!== 'undefined'"  
+                                    :alt="chosen.name" 
+                                    :src="`/siteimages/${chosen.thumbnail.path}`"
+                                    class="speaker"
+                                />
+                            </figure>
+                            <a 
+                                :class="['button  close closebtn', {'closed':open === false}]"
+                                v-on:click="close()"
+                            >
+                                <span class="icon is-small">
+                                    X
+                                </span>
+                            </a>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4">{{chosen.name}}</p>
+                            <p class="subtitle is-6">
+                                <a :href="`//twitter.com/${chosen.twitter}`">
+                                    @{{chosen.twitter}}
+                                </a><br/>
+                                {{chosen.company}}
+                            </p>
+                        </div>
+                        </div>
+                        <div v-html="chosen.talk" />
+                    </div>
                 </div>
-
-                <!-- <Speaker :data="item" /> -->
-            </div>
+           </div>
         </div>
     </div>
     </section>
+    </div>
 </template>
 
 <script>
 import bulmaCarousel from "bulma-extensions/bulma-carousel/dist/js/bulma-carousel.min.js";
 import "bulma-extensions/bulma-carousel/dist/css/bulma-carousel.min.css";
 import h2 from '@/components/h2';
+import talk from '@/components/talk';
 import { mapGetters } from 'vuex'
 
 export default {
+  data: function() {
+    return {
+        chosen: null,
+        open: false,
+    } 
+  },
+  props: {
+    items: {
+      type: Array,
+    }
+  },
   components: {
     'app-h2': h2,
+    "app-talk" : talk
   },
   mounted: function() {
     this.carousel = bulmaCarousel.attach();
-    this.$store.dispatch('speakers/get');
+    this.chosen = this.speakers[0];
+  },
+  methods: {
+    select: function (talk) {
+        this.chosen = talk;
+        this.open = !this.open;
+    },
+    close: function () {
+        this.open = !this.open;
+    }
   },
   computed: {
     ...mapGetters({
@@ -83,30 +139,13 @@ export default {
     isMobile () {
         return (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) ? true : false;
     }, 
-    filteredSpeakers() {
+     filteredSpeakers() {
          if (typeof this.speakers!== 'undefined') {
              return this.speakers.filter(speaker => {
                  return speaker.year === 2019
              })
          }
-    },
-    // speakers () {
-    //  //   if (typeof this.speakers!== 'undefined') {
-
-    //         console.log(this.speakers)
-    //     //    return this.speakers.filter(item => {
-    //     //         return (item.year === 2019)
-    //     //     }); 
-
-    //     //    return speakers.sort(function(a, b) {
-    //     //         return a.order-b.order
-    //     //     });
-
-    //     // } else {
-    //     //     return [];
-    //     // }
-    //     return [];
-    // }
+    }
   }
   
 };
@@ -114,106 +153,50 @@ export default {
 
 <style lang="sass" scoped>
     @import '~/assets/css/mq.sass';
-
-    .card-image
-       overflow: hidden
-
-    .position-absolute
-        position: absolute;
-
-    .position-relative
-        position: relative;
-        display: block;
-        z-index: 1000;
     
-    .top-100
-        top: 10px;
-    
-    .right-0
-        right: 10px;
+    a.titlebtn
+        color: $black;
+        h3 
+            min-height: 80px;
 
-    .bg-blue
-        background: $red;
-        color: white;
-
-    .label
-        width: 50%;
-        padding: 20px;
-
-    .flip-container
-        @include per(1000);
-        &:hover
-            .flipper
-                transform: rotateY(180deg)
-        .flipper 
-            transition: 0.6s;
-            transform-style: preserve-3d;
-            position: relative;
-    
-    .front, .back
-        backface-visibility: hidden;
-        position: absolute;
-        top: 0;
-        left: 0;
-    
-
-    .flip-container, .front, .back
-        width: 100%;
-        height: 21vw;
-        +mobile
-            height: 80vw;
-
-    /* front pane, placed above back */
-    .front
-        z-index: 2;
-        /* for firefox 31 */
-        transform: rotateY(0deg);
-
-    /* back, initially hidden pane */
-    .back
-        transform: rotateY(180deg);
-    
-    .back-logo
-        width: 30%;
-        height: 30%;
-        img
-            position: absolute;
-            top: 0px;
-            text-align: center;
-            eft: 35%;
-            border-radius: 50%;
-            width: 30%;
-            +mobile
-                margin: 0 auto;
-                top: 0px;
-
-    
-    .back-logo
-        width: 30%;
-        height: 30%;
-        img
-            position: absolute;
-            top: 15px;
-            text-align: center;
-            left: 35%;
-            border-radius: 50%;
-            width: 30%;
-            +mobile
-                margin: 0 auto;
-                top: 0px;
-
-    .back-content
-        padding-top: 40px;
-        font-weight: bold;
-        color: #00304a;
-        position: absolute;
-        top: 6vw;
-        left: 0;
-        right: 0;
+    .closed
+        display: none
+    img
         text-align: center;
-        font-family: Courier;
-        font-size: 0.75rem;
+        left: 30%;
+        border-radius: 50%;
+        margin: 0 auto;
+        top: 0px;
+    .spekers-card-container
+        position: fixed;
+        top: 0%;
+        left: 0%;
+        z-index: 1000;
+        width: 100%;
+        margin: 0 auto;
         +mobile
-           font-size: 0.70rem;
-           top: 16vw;
+            top: 55px;
+            left: 0%;
+            width: 100%;
+       
+        .speaker-card
+            position: absolute
+            top: 20%;
+            left: 35%;
+            width: 40%;
+            height: 470px;
+            display: block;
+            +mobile
+             top: 0px;
+             left: 0px;
+             width: 100%;
+             min-height: 230vw;
+            .closebtn
+                display: block;
+                position: absolute;
+                top: 10px;
+                right: 10px;
+        
+        .small-title
+            font-size: 12px;
 </style>
