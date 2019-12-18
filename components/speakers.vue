9ad96d16a59a39c8fd353859d82ce13b6ba5a6cc
usoghisa <usoghisa@users.noreply.github.com>
2019-12-18 01:00:44 +0000

    add card-html-bulma-css-static no db

--- components/speakers.vue
+++ components/speakers.vue
@@ -8,16 +8,66 @@
             :is-h2="true"
         >
         </app-h2>
-        <div class="columns  is-mobile is-multiline is-centered"> 
-            
-           <div 
+        <div class="columns  is-multiline "> 
+         
+<!--  <h2 class="uuu">start------ card</h2>          
+                <div class="columns  is-mobile is-multiline is-centered"> 
+                <div 
                 class="column is-three-quarters-mobile is-two-thirds-tablet
                  is-half-desktop is-one-third-widescreen is-one-quarter-fullhd"
                 v-for="item in filteredSpeakers"
                 v-bind:key="item._id"
-           >
-                    <div class="is-one  ">
-                        <h2 class="uuu">start------ card</h2>
+           > -->
+
+                <div class="column is-half-tablet is-one-third-desktop is-one-quarter-widescreen"
+                v-for="item in filteredSpeakers"
+                v-bind:key="item._id"
+                >
+                    <div class="card large round">
+                        <div class="card-image ">
+                            <figure class="image">
+                                <img src="https://source.unsplash.com/Jy6luiLBsrk" alt="Image">
+                            </figure>
+                        </div>
+                        <div class="card-content ">
+                            <div class="media">
+                                <div class="media-left">
+                                    <figure class="image is-96x96">
+                                        <img src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/profile-sample5.jpg" alt="Image">
+                                    </figure>
+                                </div>
+                                <div class="media-content">
+                                    <p class="title is-4 no-padding">Atsui</p>
+                                    <!-- <p class="title is-6"><a href="http://twitter.com/#">@twitterid</a></p> -->
+                                    <p class="subtitle is-6">Moderator</p>
+                                    <p class="title is-6"><a href="http://twitter.com/#">@twitterid</a></p>
+                                </div>
+                            </div>
+                                <div class="soc has-text-primary">
+                                    <a href="#"><i class="fa fa-github-alt fa-2x" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-youtube fa-2x" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a>
+                                    <a href="#"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a>
+                                </div>
+							<div class="outer-border"> 
+								<div class="inner-border"> 							
+									<div class="content">                                   
+										 1Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, 
+										 Lorem ipsum dolor sit amet,
+										 Lorem ipsum dolor sit amet,valueLorem ipsum dolor sit amet,
+										 Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,
+										 Lorem ipsum dolor sit amet,consectetur adipisicing elit. Dolorum consequatur numquam aliquam tenetur ad amet inventore hic beatae, quas accusantium perferendis sapiente explicabo, corporis totam! Labore reprehenderit beatae magnam animi!
+									</div>
+								</div>				
+							</div>					
+<!-- 							<footer class="card-footer is-fluid">
+								<div class="container has-text-centered"></div>
+							</footer> -->
+                        </div>
+                    </div>
+                </div>
+
+<!--                     <div class="is-one  ">
                          <div class="card-image">
                             <a 
                                 v-on:click="select(item)"
@@ -50,10 +100,22 @@
                                 </div>
                             </div>
                         </div>
-                        <h2>end card</h2>
-                    </div>
-           </div>
-           <div 
+                    </div> -->
+
+
+
+
+
+
+
+
+                    
+
+
+
+                
+<!--           </div>
+            <div 
                 :class="['column spekers-card-container modal-background', {'closed':open === false}]" 
                 v-if="this.chosen"
             >
@@ -92,7 +154,8 @@
                         <div v-html="chosen.talk" />
                     </div>
                 </div>
-           </div>
+           </div> <h2>end ------ card</h2> -->
+
         </div>
     </div>
     </section>
@@ -165,42 +228,42 @@ export default {
 
     .closed
         display: none
-    img
-        text-align: center;
-        left: 30%;
-        border-radius: 50%;
-        margin: 0 auto;
-        top: 0px;
-    .spekers-card-container
-        position: fixed;
-        top: 0%;
-        left: 0%;
-        z-index: 1000;
-        width: 100%;
-        margin: 0 auto;
-        +mobile
-            top: 55px;
-            left: 0%;
-            width: 100%;
+    
+    // img
+    //     text-align: center;
+    //     left: 30%;
+    //     border-radius: 50%;
+    //     margin: 0 auto;
+    //     top: 0px;
+    // .spekers-card-container
+    //     position: fixed;
+    //     top: 0%;
+    //     left: 0%;
+    //     z-index: 1000;
+    //     width: 100%;
+    //     margin: 0 auto;
+    //     +mobile
+    //         top: 55px;
+    //         left: 0%;
+    //         width: 100%;
        
-        .speaker-card
-            position: absolute
-            top: 20%;
-            left: 35%;
-            width: 40%;
-            height: 470px;
-            display: block;
-            +mobile
-             top: 0px;
-             left: 0px;
-             width: 100%;
-             min-height: 230vw;
-            .closebtn
-                display: block;
-                position: absolute;
-                top: 10px;
-                right: 10px;
-        
+    //     .speaker-card
+    //         position: absolute
+    //         top: 20%;
+    //         left: 35%;
+    //         width: 40%;
+    //         height: 470px;
+    //         display: block;
+    //         +mobile
+    //          top: 0px;
+    //          left: 0px;
+    //          width: 100%;
+    //          min-height: 230vw;
+    //         .closebtn
+    //             display: block;
+    //             position: absolute;
+    //             top: 10px;
+    //             right: 10px;   
         .small-title
             font-size: 12px;
 </style>
